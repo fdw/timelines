54b2d8364642e9e15a3cfb7a9771240affea22a1
Fabian Winter <5821180+fdw@users.noreply.github.com>
2019-05-31 20:02:23 +0200

    Add eras
    And aren't they pretty?

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-import json
-from collections import namedtuple
-from operator import attrgetter
 from typing import List, Dict
 
 import bokeh
 import pendulum
-from bokeh.colors import Color
 from bokeh.io import show, output_file
 from bokeh.models import BoxZoomTool, WheelPanTool, WheelZoomTool, ZoomInTool, ZoomOutTool, \
-    HoverTool, BoxAnnotation
+    HoverTool
 from bokeh.plotting import Figure
 from bokeh.plotting import figure
 
-from History import parse_json, Person, Facet
+from History import parse_json, Person, Facet, Era
 from Lanes import Lanes
-
-LANE_SPACING = 5
-HALF_LANE_HEIGHT = 1
 
 
 def create_plot() -> Figure:
@@ -48,10 +41,14 @@ def plot_facets(plot: Figure, data: Dict[str, 'Facet']):
     colors = bokeh.palettes.viridis(len(data))
     offset = 0
     for (index, facet_name) in zip(range(len(data)), data):
-        offset = plot_persons(data[facet_name].people, offset, colors[index], plot)
+        width = plot_persons(data[facet_name].people, offset, colors[index], plot)
+        plot_eras(data[facet_name].eras, offset, width, colors[index], plot)
+        offset = offset + width
 
 
-def plot_persons(persons: List[Person], offset: int, color: Color, plot: Figure) -> int:
+def plot_persons(persons: List[Person], offset: int, color: str, plot: Figure) -> int:
+    lane_padding = 3
+    lane_height = 2
     lanes = Lanes()
     for person in persons:
         lane = lanes.find_lane_ending_before(person.birth)
@@ -60,17 +57,36 @@ def plot_persons(persons: List[Person], offset: int, color: Color, plot: Figure)
         plot.quad(
             left=person.birth,
             right=person.death,
-            bottom=offset + lane * LANE_SPACING - HALF_LANE_HEIGHT,
-            top=offset + lane * LANE_SPACING + HALF_LANE_HEIGHT,
+            bottom=offset + lane * (lane_height + lane_padding) + 0.5 * lane_padding,
+            top=offset + lane * (lane_height + lane_padding) + lane_height + 0.5 * lane_padding,
             color=color,
             name=person.name
         )
 
-    return offset + lanes.size() * LANE_SPACING
+    return lanes.size() * (lane_height + lane_padding)
+
+
+def plot_eras(eras: List['Era'], offset: int, width: int, color: str, plot: Figure):
+    for era in eras:
+        plot.quad(
+            left=era.start,
+            right=era.end,
+            bottom=offset,
+            top=offset + width,
+            fill_alpha=0.1,
+            fill_color=color,
+            line_width=1,
+            line_alpha=0.2,
+            line_color=color,
+            name=era.name,
+            hatch_pattern='right_diagonal_line',
+            hatch_scale=10,
+            hatch_alpha=0.1
+        )
 
 
 if __name__ == '__main__':
-    output_file('docs/timelines.html', mode='inline')
+    output_file('docs/timelines.html', title='Timelines of the world', mode='inline')
 
     plot = create_plot()
     history = parse_json('data/data.json')
