3529c06dde2c1fd23fec82dd1ed8e81883b2a212
Fabian Winter <5821180+fdw@users.noreply.github.com>
2019-06-12 18:27:18 +0200

    Remove main function
    A main file should be enough for everyone

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from bokeh.io import show, output_file
 from History import parse_json
 from HistoryPlotter import HistoryPlotter
 
-if __name__ == '__main__':
-    output_file('docs/timelines.html', title='Timelines of the world', mode='inline')
+output_file('docs/timelines.html', title='Timelines of the world', mode='inline')
 
-    plotter = HistoryPlotter()
-    history = parse_json('data/data.json')
-    plotter.plot_facets(history.facets)
+plotter = HistoryPlotter()
+history = parse_json('data/data.json')
+plotter.plot_facets(history.facets)
 
-    plotter.finish()
+plotter.finish()
