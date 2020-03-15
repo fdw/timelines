0f7e012fa80ecf6f5d79c1e0568fa81322b528f7
Fabian Winter <5821180+fdw@users.noreply.github.com>
2020-03-15 16:51:59 +0100

    Restructure output
    - Now we have several output files :)
    - The HTML is automatically modified to include everything

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const webpack = require('webpack');
-const path = require('path')
-const MergeJsonWebpackPlugin = require('merge-jsons-webpack-plugin')
+const path = require('path');
+const MergeJsonWebpackPlugin = require('merge-jsons-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'docs')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'docs'),
   },
   devtool: 'source-map',
   resolve: {
@@ -25,7 +26,21 @@ module.exports = {
       ]
   },
   optimization: {
-    usedExports: true
+    usedExports: true,
+    splitChunks: {
+      chunks: 'all',
+      minSize: 0,
+      cacheGroups: {
+        data: {
+          test: /\.json$/,
+          filename: '[name].js',
+          name(module) {
+            const filename = module.rawRequest.replace(/^.*[\\/]/, '');
+            return filename.substring(0, filename.lastIndexOf('.'));
+          },
+        }
+      }
+    }
   },
   plugins: [
     new MergeJsonWebpackPlugin({
@@ -38,6 +53,11 @@ module.exports = {
         ]
       }
     }),
-    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /a^/)
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /a^/),
+    new HtmlWebpackPlugin({
+      template: "src/timelines.html",
+      inject: 'body',
+      filename: "timelines.html"
+    })
   ]
 }
