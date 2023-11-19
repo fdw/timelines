80c39ba4b18bc096ccf63cff36a79c9bd2307d3b
Fabian Winter <5821180+fdw@users.noreply.github.com>
2023-11-19 16:01:06 +0100

    Try a different layout algorithm
    Instead of only adding new lines at the bottom, switch between top and
    bottom. This avoid cascades and adds symmetry.
    
    It's still not perfect, tho.

--- src/components/utils/Lanes.tsx
+++ src/components/utils/Lanes.tsx
@@ -5,33 +5,60 @@ import { sortByDate } from '../../timeUtil'
 import { EventVis } from '../visualizations/EventVis'
 import { PersonVis } from '../visualizations/PersonVis'
 
-export function Lanes({ people = [], events=[] }: { people?: Person[], events?: Event[] }): React.ReactElement {
+export function Lanes({ people = [], events = [] }: { people?: Person[], events?: Event[] }): React.ReactElement {
   const lanes: (Visualizable)[][] = []
 
+  let nextLane: 'up' | 'down' = 'up'
+
   function findLaneEndingBefore(date: DateTime): number {
-    for (const lane in Object.keys(lanes)) {
-      if (laneEnd(Number(lane)).plus({ years: 5 }) < date) {
-        return Number(lane)
-      }
-    }
-    return addLane()
+    return Object.keys(lanes).map(it => ({
+      index: Number(it),
+      date: laneEnd(Number(it)),
+    })).filter(it => it.date.plus({ years: 5 }) < date).sort(sortByDate)[0]?.index ?? addLane()
   }
 
   function laneEnd(index: number): DateTime {
     const lastObject = lanes[index][lanes[index].length - 1]
-    switch(lastObject.type) {
-      case 'Person': return lastObject.object.death
-      case 'Event': return lastObject.object.date.plus({years: 10})
+
+    switch (lastObject.type) {
+      case 'Person':
+        return lastObject.object.death
+      case 'Event':
+        return lastObject.object.date.plus({ years: 10 })
     }
   }
 
+
   function addLane(): number {
-    const newSize = Object.keys(lanes).length
-    lanes[newSize] = []
-    return newSize
+    const nextIndex = findIndex()
+    lanes[nextIndex] = []
+    return nextIndex
   }
 
-  const everything = people.map(it => ({date: it.birth, object: it, type: 'Person'} as Visualizable)).concat(events.map(it => ({date: it.date, object: it, type: 'Event'}))).sort(sortByDate)
+  function findIndex(): number {
+    if (lanes.length === 0) {
+      return 10
+    }
+
+    let fn: (_: number[]) => number
+    switch (nextLane) {
+      case 'up':
+        fn = (values: number[]) => Math.min(...values) - 1
+        nextLane = 'down'
+        break
+      case 'down':
+        fn = (values: number[]) => Math.max(...values) + 1
+        nextLane = 'up'
+        break
+    }
+    return fn(Object.keys(lanes).map(it => Number(it)))
+  }
+
+  const everything = people.map(it => ({
+    date: it.birth,
+    object: it,
+    type: 'Person',
+  } as Visualizable)).concat(events.map(it => ({ date: it.date, object: it, type: 'Event' }))).sort(sortByDate)
 
   return (
     <>
@@ -44,10 +71,12 @@ export function Lanes({ people = [], events=[] }: { people?: Person[], events?: 
   )
 }
 
-function getVisualization({object, type}: Visualizable, offset: number): React.ReactElement {
-  switch(type) {
-    case 'Person': return <PersonVis key={object.name} offset={offset} person={object} />
-    case 'Event': return <EventVis event={object} key={object.title} offset={offset} />
+function getVisualization({ object, type }: Visualizable, offset: number): React.ReactElement {
+  switch (type) {
+    case 'Person':
+      return <PersonVis key={object.name} offset={offset} person={object} />
+    case 'Event':
+      return <EventVis event={object} key={object.title} offset={offset} />
   }
 }
 
